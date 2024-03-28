460d53185e4fe5e16f3f625c2ee874dbf5fe9a47
MetaMelchoir <ctabere@users.noreply.github.com>
2024-03-27 23:10:53 -0400

    started js form project

--- JS/test.js
+++ JS/test.js
@@ -1,56 +1,61 @@
-var color = 'purple';
-const colorTwo = 'green';
-let colorThree = 'blue';
+// var color = 'purple';
+// const colorTwo = 'green';
+// let colorThree = 'blue';
 
-console.log(color);
-console.log(colorTwo);
-console.log(colorThree);
+// console.log(color);
+// console.log(colorTwo);
+// console.log(colorThree);
 
-color = 'brown';
-console.log(color);
-console.log(color);
-
-color = 'yellow';
-console.log(color);
-
-// color = 2;
+// color = 'brown';
+// console.log(color);
 // console.log(color);
 
-// colorTwo = 2;
-// console.log(colorTwo);
+// color = 'yellow';
+// console.log(color);
 
-colorThree = 33;
-console.log(colorThree);
+// // color = 2;
+// // console.log(color);
 
-const myList = [1, 2, 3, 4, color, 6, 7];
-myList.shift()
-console.log(myList[2]+myList[4]);
-console.log(typeof(myList[2]+myList[4]));
-console.log(myList);
-myList.push(8, 9, 10); 
-console.log(myList);
-console.log(myList.pop());
-console.log(myList);
+// // colorTwo = 2;
+// // console.log(colorTwo);
+
+// colorThree = 33;
+// console.log(colorThree);
+
+// const myList = [1, 2, 3, 4, color, 6, 7];
+// myList.shift()
+// console.log(myList[2]+myList[4]);
+// console.log(typeof(myList[2]+myList[4]));
+// console.log(myList);
+// myList.push(8, 9, 10); 
+// console.log(myList);
+// console.log(myList.pop());
+// console.log(myList);
 
 
 
-import assert from "assert";
+// import assert from "assert";
+
+// function findRemainder(dividend: number, divisor: number): number {
+//     dividend/divisor
+//     return %;
+// }
+
+// console.log("Example:");
+// console.log(findRemainder(3, 2));
+
+// // These "asserts" are used for self-checking
+// assert.strictEqual(findRemainder(10, 3), 1);
+// assert.strictEqual(findRemainder(14, 4), 2);
+// assert.strictEqual(findRemainder(27, 4), 3);
+// assert.strictEqual(findRemainder(10, 5), 0);
+// assert.strictEqual(findRemainder(10, 1), 0);
+// assert.strictEqual(findRemainder(5, 7), 5);
+// assert.strictEqual(findRemainder(7, 5), 2);
+
+// console.log("Coding complete? Click 'Check Solution' to earn rewards!");
 
 function findRemainder(dividend: number, divisor: number): number {
-    dividend/divisor
-    return %;
+    const remainder  dividend/divisor;
+    return remainder;
 }
-
-console.log("Example:");
-console.log(findRemainder(3, 2));
-
-// These "asserts" are used for self-checking
-assert.strictEqual(findRemainder(10, 3), 1);
-assert.strictEqual(findRemainder(14, 4), 2);
-assert.strictEqual(findRemainder(27, 4), 3);
-assert.strictEqual(findRemainder(10, 5), 0);
-assert.strictEqual(findRemainder(10, 1), 0);
-assert.strictEqual(findRemainder(5, 7), 5);
-assert.strictEqual(findRemainder(7, 5), 2);
-
-console.log("Coding complete? Click 'Check Solution' to earn rewards!");
