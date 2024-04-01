c30f09ed7cd14784511d0a19d3a278b382034fae
MetaMelchoir <ctabere@users.noreply.github.com>
2024-03-31 23:53:46 -0400

    comments

--- Web Form Assignment/form.js
+++ Web Form Assignment/form.js
@@ -2,15 +2,15 @@
 const firstName = document.getElementById('firstname');
 firstName.addEventListener('change', printChange);  
 
-// 
+// target last name id
 const lastName = document.getElementById('lastname');
 lastName.addEventListener('change', printChange);  
 
-//
+// target city id
 const city = document.getElementById('city');
 city.addEventListener('change', printChange);  
 
-//
+// 
 function printChange(e) {
   console.log(e.target.value);
 }
