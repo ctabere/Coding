7e58067ee5ee5a18e7efc0eeb799bef1b617f4f8
MetaMelchoir <ctabere@users.noreply.github.com>
2024-04-03 18:11:08 -0400

    assignment work

--- Web Form Assignment/form.js
+++ Web Form Assignment/form.js
@@ -1,16 +1,27 @@
-// target first name id 
-const firstName = document.getElementById('firstname');
-firstName.addEventListener('change', printChange);  
+// // target first name id 
+// const firstName = document.getElementById('firstname');
+// firstName.addEventListener('change', printChange);  
 
-// target last name id
-const lastName = document.getElementById('lastname');
-lastName.addEventListener('change', printChange);  
+// // target last name id
+// const lastName = document.getElementById('lastname');
+// lastName.addEventListener('change', printChange);  
 
-// target city id
-const city = document.getElementById('city');
-city.addEventListener('change', printChange);  
+// // target city id
+// const city = document.getElementById('city');
+// city.addEventListener('change', printChange);  
 
-// 
-function printChange(e) {
-  console.log(e.target.value);
+// // 
+// function printChange(e) {
+//   console.log(e.target.value);
+// }
+
+
+    
+
+function submitButton() {
+    const firstName = document.getElementById('firstname').value;
+    const lastName = document.getElementById('lastname').value;
+    const city = document.getElementById('city').value;
+    const gender = document.getElementsByClassName('options').value;
+    console.log(firstName, lastName, city, gender);
 }
