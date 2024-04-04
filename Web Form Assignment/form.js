1415f9e47840c24a8f5f36a0c5bc9c642f83ab9a
MetaMelchoir <ctabere@users.noreply.github.com>
2024-04-04 15:58:32 -0400

    more work on for loop

--- Web Form Assignment/form.js
+++ Web Form Assignment/form.js
@@ -1,27 +1,33 @@
-// // target first name id 
+// // target first name id
 // const firstName = document.getElementById('firstname');
-// firstName.addEventListener('change', printChange);  
+// firstName.addEventListener('change', printChange);
 
 // // target last name id
 // const lastName = document.getElementById('lastname');
-// lastName.addEventListener('change', printChange);  
+// lastName.addEventListener('change', printChange);
 
 // // target city id
 // const city = document.getElementById('city');
-// city.addEventListener('change', printChange);  
+// city.addEventListener('change', printChange);
 
-// // 
+// //
 // function printChange(e) {
 //   console.log(e.target.value);
 // }
 
-
-    
-
 function submitButton() {
-    const firstName = document.getElementById('firstname').value;
-    const lastName = document.getElementById('lastname').value;
-    const city = document.getElementById('city').value;
-    const gender = document.getElementsByClassName('options').value;
-    console.log(firstName, lastName, city, gender);
+  const firstName = document.getElementById("firstname").value;
+  const lastName = document.getElementById("lastname").value;
+  const city = document.getElementById("city").value;
+  const genderBox = document.getElementsByName("gender");
+
+   function validateGender() {
+       for (let i = 0; i < genderBox.length; i++) {
+           if (i.checked == true )
+               console.log(genderBox);
+           }
+
+       }
+
+  console.log(firstName, lastName, city, genderBox);
 }
