d0491e73036ff6781a5bba26e44a8da109885fb5
MetaMelchoir <ctabere@users.noreply.github.com>
2024-04-06 03:16:41 -0400

    array

--- Web Form Assignment/form.js
+++ Web Form Assignment/form.js
@@ -15,19 +15,29 @@
 //   console.log(e.target.value);
 // }
 
+
+
 function submitButton() {
-  const firstName = document.getElementById("firstname").value;
-  const lastName = document.getElementById("lastname").value;
-  const city = document.getElementById("city").value;
-  const genderBox = document.getElementsByName("gender");
+   const genderBox = document.getElementsByName("gender");
+   const firstName = document.getElementById("firstname").value;
+   const lastName = document.getElementById("lastname").value;
+   const city = document.getElementById("city").value;
+
 
    function validateGender() {
-       for (let i = 0; i < genderBox.length; i++) {
-           if (i.checked == true )
-               console.log(genderBox);
+      // let isChecked = false;
+       for (let i = 0; i < 2; i++) {
+           if (genderBox = isChecked)
+               console.log(genderBox[i].value);
            }
 
        }
 
-  console.log(firstName, lastName, city, genderBox);
-}
+  console.log(firstName, lastName, city);}
+
+
+  const myArray = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+
+  const myMap = myArray.map(x => x / x);
+
+  console.log(myMap);
