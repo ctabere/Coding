fa9dc110f3eba0f465113409655a4c20a4fea2d4
MetaMelchoir <ctabere@users.noreply.github.com>
2024-04-17 20:41:34 -0400

    set up sudoku board

--- Countdown Timer/script.js
+++ Countdown Timer/script.js
@@ -1,6 +1,7 @@
-document.getElementById("startButton").addEventListener("click"), function() {
+document.getElementById("startButton").addEventListener("click"),
+  function () {
     const timerDisplay = document.getElementById("timer");
-    let seconds = 20;}
+    let seconds = 20;
+  };
 
-    
 // write out all the steps
