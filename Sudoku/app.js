d6cdc7ee6df71868c15ba96a69ac2313f0cf385e
MetaMelchoir <ctabere@users.noreply.github.com>
2024-04-18 18:59:21 -0400

    put jquery before js

--- Sudoku/app.js
+++ Sudoku/app.js
@@ -1,17 +1,17 @@
 // jquery
 $(document).ready(function(){
   $(".grid").children().each(function(){  //  selecting the children of the grid div, so all the cells are children
-      var x; // tried var, let, and const
-      var y;
+      let x; // tried var, let, and const
+      let y;
 
       x = parseInt($(this).attr("id").substring(0,1)); // converting the grid sting into integers
       y = parseInt($(this).attr("id").substring(1));
       // border of 3x3 cells
       if (y == 2 || y == 5){
-        $(this).css("border-right-width", "10px"); // thicken border in css for the board to take shape
+        $(this).css("border-right-width", 8); // thicken border in css for the board to take shape
       }
       if (x == 2 || x == 5){
-        $(this).css("border-bottom-width", "10px");
+        $(this).css("border-bottom-width", 8);
       }
     });
 });
