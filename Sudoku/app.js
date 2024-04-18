80914c9d6a7ca73cb085822fa7f31c315d91e72b
MetaMelchoir <ctabere@users.noreply.github.com>
2024-04-18 18:23:05 -0400

    imported jquery 3.7.1

--- Sudoku/app.js
+++ Sudoku/app.js
@@ -1,18 +1,17 @@
 // jquery
-$(document).ready(function () {
-  $(".grid")
-    .children()
-    .each(function () {
-      var x;
+$(document).ready(function(){
+  $(".grid").children().each(function(){  //  selecting the children of the grid div, so all the cells are children
+      var x; // tried var, let, and const
       var y;
-      x = parseInt($(this).attr("id").substring(0, 1));
+
+      x = parseInt($(this).attr("id").substring(0,1)); // converting the grid sting into integers
       y = parseInt($(this).attr("id").substring(1));
       // border of 3x3 cells
       if (y == 2 || y == 5){
-        $(this).css("border-right-width", 10);
+        $(this).css("border-right-width", "10px"); // thicken border in css for the board to take shape
       }
       if (x == 2 || x == 5){
-        $(this).css("border-bottom-width", 10);
+        $(this).css("border-bottom-width", "10px");
       }
     });
 });
