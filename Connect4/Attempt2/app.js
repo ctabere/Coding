2996c5e2530c4fec46aa6d80cf43b06c16e67672
MetaMelchoir <ctabere@users.noreply.github.com>
2024-06-10 23:09:58 -0400

    mon

--- Connect4/Attempt2/app.js
+++ Connect4/Attempt2/app.js
@@ -1,13 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const pieces = document.querySelectorAll('.piece')
-    const columns = document.querySelectorAll('.game-buttons')
-  
-    // Add cells to each column
-    columns.forEach(column => {
-      for (let i = 0; i < 6; i++) {
-        // Assuming a 6-row grid
-        const cell = document.createElement('div')
-        cell.classList.add('cell')
-        column.appendChild(cell)
-      }
-    })})
+document.addEventListener("DOMContentLoaded", () => {
+  const pieces = document.querySelectorAll(".game-buttons");
+  const columns = document.querySelectorAll(".column");
+
+  // Add cells to each column
+  columns.forEach((column) => {
+    for (let i = 0; i < 6; i++) {
+      // Assuming a 6-row grid
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      column.appendChild(cell);
+    }
+  });
+});
